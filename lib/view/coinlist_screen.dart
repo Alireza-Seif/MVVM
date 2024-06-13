11079509897dc332038859db236d0318e396af59
alirezaseif20 <alirezaariye@gmail.com>
2024-06-13 18:38:12 +0330

    feat: add view (CoinListScreen)

---
+++ lib/view/coinlist_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm/view_model/coinlist_viewmmodel.dart';
+import 'package:provider/provider.dart';
+
+class CoinListScreen extends StatefulWidget {
+  const CoinListScreen({super.key});
+
+  @override
+  State<CoinListScreen> createState() => _CoinListScreenState();
+}
+
+class _CoinListScreenState extends State<CoinListScreen> {
+  final _viewMModel = CoinlistViewmodel();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _viewMModel.loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => _viewMModel,
+        child:
+            Consumer<CoinlistViewmodel>(builder: (context, viewModel, child) {
+          return getUi(viewModel);
+        }));
+  }
+
+  Widget getUi(CoinlistViewmodel viewModel) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: viewModel.cryptoList.length ?? 0,
+        itemBuilder: (context, index) {
+          return Text(viewModel.cryptoList[index].name ?? '');
+        },
+      ),
+    );
+  }
+}
